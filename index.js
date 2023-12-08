c57175fe43b9a8381f15a9dda5492dfc7e75468a
ky5burke <ky5burke@gmail.com>
2023-12-08 16:02:06 -0600

    passing all seven tests over object functions

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+const employee = {name: "Ky Burke", streetaddress: "710 Franklin Dr"};
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newEmployee = {...employee };
+
+    newEmployee[key] = value
+
+    return newEmployee
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value
+    return employee
+}
+
+function deleteFromEmployeeByKey(employee, name) {
+    let newEmpl = {...employee};
+    
+    delete newEmpl.name;
+    
+    return newEmpl
+}
+
+console.log(deleteFromEmployeeByKey(employee, "Ky Burke"))
+
+function destructivelyDeleteFromEmployeeByKey(employee, name) {
+    delete employee.name;
+    return employee
+}
